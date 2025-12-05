11777f17b1ab2693db8d67362f7ee171f9161707
jeetpatil12 <90495511+jeetpatil12@users.noreply.github.com>
2025-12-05 21:14:32 +0530

    Update WhatsAppButton.js

--- src/components/WhatsAppButton.js
+++ src/components/WhatsAppButton.js
@@ -1,7 +1,7 @@
 export default function WhatsAppButton() {
   return (
     <a
-      href="https://wa.me/919999999999"
+      href="https://wa.me/918600245219"
       className="whatsapp-float"
       target="_blank"
       rel="noreferrer"
